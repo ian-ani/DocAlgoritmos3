536c521e97df96eb3e52e97b0c78004ae0110a4b
Iris <irisaunav@gmail.com>
2026-03-14 18:44:23 +0100

    Arreglar librerías de JUnit

--- Algoritmos/src/es/iesmz/ed/algoritmos/PolidivisibleTest.java
+++ Algoritmos/src/es/iesmz/ed/algoritmos/PolidivisibleTest.java
@@ -2,9 +2,9 @@ package es.iesmz.ed.algoritmos;
 
 /* LIBRERIAS */
 
-import org.testng.annotations.Test;
-import static org.testng.AssertJUnit.assertFalse;
-import static org.testng.AssertJUnit.assertTrue;
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests unitarios de Polidivisible
